354ad56e8028a199a58d6334540c1facfb2bc49f
anderson <nobreanderson@hotmail.com>
2024-01-24 20:39:21 -0300

    adção de imagem

--- main.js
+++ main.js
@@ -10,5 +10,18 @@ $(document).ready(function() {
 
     $('form').on('submit', function(e) {
         e.preventDefault()
+        const enderecoDaNovaImagem = $('#endereco-imagem-nova').val()
+        const novoItem = $('<li></li>')
+        $(`img src="${enderecoDaNovaImagem}" />`).appendTo(novoItem)
+        
+        $(`
+            <div class="overlay-image-link">
+                    <a href="${enderecoDaNovaImagem}" target="_blank" title="Ver Imagem em Tamanho Real">
+                        Ver Imagem em Tamanho Real
+                    </a>
+            </div>
+        `).appendTo(novoItem)
+        $(novoItem).appendTo('ul')
+        $('#endereco-imagem-nova').val('')
     })
 })
